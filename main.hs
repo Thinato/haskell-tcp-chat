e8243cc2e21118ac60f5f881ceda47e5705108cb
Paulo Lisecki <piokinovaes@gmail.com>
2025-04-15 22:29:54 -0300

    basic things

--- main.hs
+++ main.hs
@@ -1,2 +1,15 @@
 main :: IO ()
-main = putStrLn "Hello World"
+main = do
+    putStrLn "Hello World"
+    putStrLn $ greet "Paulo"
+    putStrLn $ "Double 4 is " ++ show (double 4)
+
+greet :: String -> String
+greet name = "Hello " ++ name
+
+double :: Int -> Int
+double 0 = 0 
+double x = x * 2 
+-- this is pattern matching
+-- if the argument is 0, it will just return 0
+-- and 0 * 2 will not be evaluated
